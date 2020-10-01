e529b52f339cc34517a8fdb22ec067c1bbccc053
liatdavid2 <liatdavid2@gmail.com>
2020-10-01 04:21:40 +0300

    Appmodel

--- MvcApplication1/AppModel.Designer.cs
+++ MvcApplication1/AppModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Mvc mangament system\MvcApplication1\CompanyModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Mvc mangament system\MvcApplication1\AppModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
